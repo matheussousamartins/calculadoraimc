bd2dab53925c1649151a4b9c94e51cae73c4e77e
Matheus Sousa Martins <matheusmartins0709@gmail.com>
2025-02-02 18:04:58 -0300

    Revert "Melhorando experiência"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.comunidadedevspace.imc"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
